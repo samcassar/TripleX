c513bb729b321ce89bcd2f73ece53083689f4891
Sam <cassarsam2@gmail.com>
2020-07-02 18:50:10 -0700

    Fleshing Out Even More
    No lives added, should be on main's timeline

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <ctime>
 
-int CodeA; int CodeB; int CodeC; int checkBranchUpdates;
-int GuessA; int GuessB; int GuessC; int GuessSum; int GuessProduct; bool GuessCorrect;
+int CodeA; int CodeB; int CodeC;
+int GuessA; int GuessB; int GuessC; int GuessSum; int GuessProduct; bool GuessCorrect; 
 void PlayGame(); void PrintIntroduction(); bool BattleAgain(); int Choice; void IsGuessCorrect();
+int RiddleCount = 1; int RandNumGenerator(int RiddleCount); int RiddleLimit = 6;
 
 int main()
 {   
@@ -12,6 +14,8 @@ int main()
 }
 
 void PlayGame() {
+    srand(time(NULL)); //rando magic
+
     PrintIntroduction();
 
     IsGuessCorrect();
@@ -30,26 +34,34 @@ void PrintIntroduction() {
     std::cout << "\nAs your friend scowls in pain from the fallen royal hangman's blows,\n";
     std::cout << "You dare defy his stature, and challenge him to a chivalirous acuity duel of the traditional and deadly Royal Hangman game\n";
     std::cout << "However, he commands you to play a more civilized game of arthimetic rather then semantics\n";
-    std::cout << "He declares \"thy must answer my five arthimitic riddles, each tougher than the last, thy shall only possess 3 guesses and a minute for each riddle.\n";
+    std::cout << "He declares \"thy must answer my " << RiddleLimit << " arthimitic riddles, each tougher than the last, thy shall only possess 3 guesses and a minute for each riddle.\n";
     std::cout << "Only then will your friend be free from anguish and I will face Robespierre's blade; otherwise thy and your peasant friend will perish!\n\n";
-    std::cout << "For the first riddle, ";
+    
 }
 
 void IsGuessCorrect() {
+    RiddleCount++;
 
-    int CodeSum = CodeA + CodeB + CodeC; int CodeProduct = CodeA * CodeB * CodeC; std::string RiddleCount = "first HOW TODO LIST OF stuff";
+    //Generates this riddle's code
+    CodeA = RandNumGenerator(RiddleCount); CodeB = RandNumGenerator(RiddleCount); CodeC = RandNumGenerator(RiddleCount);
+
+    int CodeSum = CodeA + CodeB + CodeC; int CodeProduct = CodeA * CodeB * CodeC; 
     
-    
-    std::cout << "name the three numbers which simultaneously possess a sum of " << CodeSum << " and a product of " << CodeProduct << "\n";
+    std::cout << "For riddle #" << RiddleCount;
+    std::cout << "name the three numbers which simultaneously possess:\n\n A sum of " << CodeSum << " and A product of " << CodeProduct << "\n\n";
     std::cout << "Guess three numbers each seperated by a space in the line below:\n";
 
     std::cin >> GuessA >> GuessB >> GuessC;
-    std::cout << "Thy guess of the three numbers is: (" << GuessA << ", " << GuessB << ", " << GuessC << ")\n\n";
 
     GuessSum = GuessA + GuessB + GuessC;  GuessProduct = GuessA * GuessB * GuessC;
 
     GuessCorrect = (CodeSum == GuessSum && CodeProduct == GuessProduct);
+
+    if (GuessCorrect) {}
+
 }
+
+int RandNumGenerator(int RiddleCount) { rand() % RiddleCount   + RiddleCount;} //when | (RC = 2, [3,2]), (RC = 3, [5,3]), (RC = 4, [7,4]), (RC = 5, [9,5]), (RC = 6, [11,6]), etc |
 
 bool BattleAgain() {
     std::cout << "Oh no the dishonored hangman somehow survived and has grabbed another one of your friends! What do you do?\n\n";
@@ -58,3 +70,20 @@ bool BattleAgain() {
     std::cout << "Input 1 or 2 in the line below to choose your option:\n"; std::cin >> Choice;
     if (Choice == 1) {return true;} else {return false;}
 }
+
+/* TODO:
+DONE -Make each CodeA/b/c into a random number 
+DONE -make RiddleCount increase per next riddle
+DONE -set up level difficulty to scale the range and difficulty of randomNumGen
+-Go through the program in your head to make sure Riddle# checks out
+
+
+
+
+Misc:
+-Add a RiddleCount/RiddleLimit for each riddle
+-add num of guesses left that updates every wrong guess
+-add a state where if guesses == 0 then game go to the ending BattleAgain()
+-create a loss BattleAgain() ending
+
+*/
